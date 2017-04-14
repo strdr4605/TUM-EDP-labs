381d19b26a83e9a7f643f312ca4202f8d683028e
strdr4605 <str.dr4605@gmail.com>
2017-04-14 13:38:47 +0300

    add drawLines

--- lab#3/EDP-lab#3/app/src/main/java/com/example/strainu/edp_lab3/DrawView.java
+++ lab#3/EDP-lab#3/app/src/main/java/com/example/strainu/edp_lab3/DrawView.java
@@ -9,6 +9,8 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
+import java.util.Random;
+
 /**
  * Created by strainu on 4/14/17.
  */
@@ -23,11 +25,25 @@ public class DrawView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        drawLines(5, canvas);
+    }
+
+    private void drawLines(int n, Canvas canvas) {
+        Random rnd = new Random();
         paint = new Paint();
-        paint.setColor(Color.BLACK);
         paint.setStrokeWidth(10);
-        canvas.drawLine(0, 0, 100, 100, paint);
-        canvas.drawLine(100, 0, 0, 100, paint);
+        float startX = 0;
+        float startY = 0;
+        float stopX = 0;
+        float stopY = 0;
+        for(int i = 0; i < n; i++) {
+            paint.setARGB(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
+            startX = i * 50;
+            startY = i * 10;
+            stopX = startX + 100;
+            stopY = startY + 200;
+            canvas.drawLine(30 + startX, 30 + startY, stopX, stopY, paint);
+        }
     }
 
 }
