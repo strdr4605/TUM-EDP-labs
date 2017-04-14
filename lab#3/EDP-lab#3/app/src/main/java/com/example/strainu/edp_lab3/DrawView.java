33c1863374e69dc112e70efa4b1e604849d6105f
strdr4605 <str.dr4605@gmail.com>
2017-04-14 13:50:15 +0300

    add drawBezierCurve

--- lab#3/EDP-lab#3/app/src/main/java/com/example/strainu/edp_lab3/DrawView.java
+++ lab#3/EDP-lab#3/app/src/main/java/com/example/strainu/edp_lab3/DrawView.java
@@ -5,6 +5,7 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
@@ -17,6 +18,7 @@ import java.util.Random;
 
 public class DrawView extends View {
     Paint paint;
+    Path path;
 
     public DrawView(Context context) {
         super(context);
@@ -26,6 +28,7 @@ public class DrawView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         drawLines(5, canvas);
+        drawBezierCurve(canvas);
     }
 
     private void drawLines(int n, Canvas canvas) {
@@ -38,12 +41,28 @@ public class DrawView extends View {
         float stopY = 0;
         for(int i = 0; i < n; i++) {
             paint.setARGB(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
-            startX = i * 50;
+            startX = 700 + i * 50;
             startY = i * 10;
             stopX = startX + 100;
             stopY = startY + 200;
             canvas.drawLine(30 + startX, 30 + startY, stopX, stopY, paint);
         }
+    }
+
+    private void drawBezierCurve(Canvas canvas) {
+        paint.setStyle(Paint.Style.STROKE);
+        path = new Path();
+        paint.setColor(Color.RED);
+        paint.setStrokeWidth(6);
+        path.moveTo(54, 35);
+        path.cubicTo(102, 120, 350, 100, 237, 18);
+        canvas.drawPath(path, paint);
+
+        paint.setColor(Color.BLUE);
+        paint.setStrokeWidth(10);
+        path.moveTo(79, 385);
+        path.cubicTo(102, 120, 310, 476, 427, 232);
+        canvas.drawPath(path, paint);
     }
 
 }
